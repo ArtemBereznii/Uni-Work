b3319a48cff095339c6b252f17b37333563d6c0f
ArtemBereznii <artem.berezniy8@gmail.com>
2024-09-28 20:04:51 +0300

    Improved

--- practicals/2.1/2.1.cpp
+++ practicals/2.1/2.1.cpp
@@ -4,8 +4,11 @@
 #include <ctime>
 #include <iomanip>  
 
-int main()
-{
+int main() {
+    int task;
+    std::cout << "Enter the task (2-4) you want to run : ";
+    std::cin >> task;
+
     srand(static_cast<unsigned>(time(0)));  // Initialize random number generator
 
     int m = 10;  // Number of sequences
@@ -29,112 +32,113 @@ int main()
         }
         std::cout << std::endl;
     }
-
-    // Task 2
-    /*
-    // Search for the maximum value by absolute value
-    int min_value = A[0][0]; 
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (value < min_value) {
-                min_value = value;
-            }
-        }
-    }
-
-
-    for (int row = 0; row < A.size(); row++) {
-        for (int col = 0; col < A[row].size(); col++) {
-            if (A[row][col] == min_value) {
-                std::cout << "Element " << min_value << " is located: "
-                    << "row " << row + 1 << ", column " << col + 1 << std::endl;
-            }
-        }
-    }
-*/
-
-    // Task 3
-    /*
-    int min_value = A[0][0];
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (value < min_value) {
-                min_value = value;
-            }
-        }
-    }
-
-    for (int row = 0; row < A.size(); row++) {
-        for (int col = 0; col < A[row].size(); col++) {
-            if (A[row][col] == min_value) {
-                std::cout << "Element " << min_value << " is located: "
-                    << "row " << row + 1 << ", column " << col + 1 << std::endl;
-            }
-        }
-    }
-
-    int max_value = min_value;  // Initialize with the first element
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (value < 0) {
-                 if (value > max_value) {
-                    max_value = value;
-                 }
-            }
-           
-        }
-    }
-
-    for (int row = 0; row < A.size(); row++) {
-        for (int col = 0; col < A[row].size(); col++) {
-            if (A[row][col] == max_value) {
-                std::cout << "Element " << max_value << " is located: "
-                    << "row " << row + 1 << ", column " << col + 1 << std::endl;
-            }
-        }
-    }
-*/
-
-    // Task 4
-
-    int max_value = A[0][0];
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (value > max_value) {
-                max_value = value;
-            }
-        }
-    }
-
-    for (int row = 0; row < A.size(); row++) {
-        for (int col = 0; col < A[row].size(); col++) {
-            if (A[row][col] == max_value) {
-                std::cout << "Element " << max_value << " is located: "
-                    << "row " << row + 1 << ", column " << col + 1 << std::endl;
-            }
-        }
-    }
-
-    int min_value = max_value;  // Initialize with the first element
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (value > 0) {
+    if (task == 2) {
+        // Task 2
+        // Search for the maximum value by absolute value
+        int min_value = A[0][0];
+        for (const std::vector<int>& sequence : A) {
+            for (int value : sequence) {
                 if (value < min_value) {
                     min_value = value;
                 }
             }
-
         }
-    }
 
-    for (int row = 0; row < A.size(); row++) {
-        for (int col = 0; col < A[row].size(); col++) {
-            if (A[row][col] == min_value) {
-                std::cout << "Element " << min_value << " is located: "
-                    << "row " << row + 1 << ", column " << col + 1 << std::endl;
+
+        for (int row = 0; row < A.size(); row++) {
+            for (int col = 0; col < A[row].size(); col++) {
+                if (A[row][col] == min_value) {
+                    std::cout << "Element " << min_value << " is located: "
+                        << "row " << row + 1 << ", column " << col + 1 << std::endl;
+                }
             }
         }
     }
+    else if (task == 3) {
+        // Task 3
+        int min_value = A[0][0];
+        for (const std::vector<int>& sequence : A) {
+            for (int value : sequence) {
+                if (value < min_value) {
+                    min_value = value;
+                }
+            }
+        }
 
-    return 0;
+        for (int row = 0; row < A.size(); row++) {
+            for (int col = 0; col < A[row].size(); col++) {
+                if (A[row][col] == min_value) {
+                    std::cout << "Element " << min_value << " is located: "
+                        << "row " << row + 1 << ", column " << col + 1 << std::endl;
+                }
+            }
+        }
+
+        int max_value = min_value;  // Initialize with the first element
+        for (const std::vector<int>& sequence : A) {
+            for (int value : sequence) {
+                if (value < 0) {
+                    if (value > max_value) {
+                        max_value = value;
+                    }
+                }
+
+            }
+        }
+
+        for (int row = 0; row < A.size(); row++) {
+            for (int col = 0; col < A[row].size(); col++) {
+                if (A[row][col] == max_value) {
+                    std::cout << "Element " << max_value << " is located: "
+                        << "row " << row + 1 << ", column " << col + 1 << std::endl;
+                }
+            }
+        }
+    }
+    else if (task == 4) {
+
+        // Task 4
+
+        int max_value = A[0][0];
+        for (const std::vector<int>& sequence : A) {
+            for (int value : sequence) {
+                if (value > max_value) {
+                    max_value = value;
+                }
+            }
+        }
+
+        for (int row = 0; row < A.size(); row++) {
+            for (int col = 0; col < A[row].size(); col++) {
+                if (A[row][col] == max_value) {
+                    std::cout << "Element " << max_value << " is located: "
+                        << "row " << row + 1 << ", column " << col + 1 << std::endl;
+                }
+            }
+        }
+
+        int min_value = max_value;  // Initialize with the first element
+        for (const std::vector<int>& sequence : A) {
+            for (int value : sequence) {
+                if (value > 0) {
+                    if (value < min_value) {
+                        min_value = value;
+                    }
+                }
+
+            }
+        }
+
+        for (int row = 0; row < A.size(); row++) {
+            for (int col = 0; col < A[row].size(); col++) {
+                if (A[row][col] == min_value) {
+                    std::cout << "Element " << min_value << " is located: "
+                        << "row " << row + 1 << ", column " << col + 1 << std::endl;
+                }
+            }
+        }
+    }
+    else {
+        std::cout << "The task doesn`t exist, try 2-4";
+    }
 }
